0e70650a5fdeb5327366dd5d540ae9c4f92a0464
KennyWks <kenny.perulu@gmail.com>
2021-08-04 17:58:56 +0800

    set again new url for mobile

--- src/helpers/CRUD.js
+++ src/helpers/CRUD.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const ApiURL = 'http://10.0.2.2:81/BetaBajual.com/public';
+// export const ApiURL = 'http://10.0.2.2:81/BetaBajual.com/public';
+export const ApiURL = 'http://thonbers.rf.gd';
 
 const header = {
   'Content-Type': 'multipart/form-data',
